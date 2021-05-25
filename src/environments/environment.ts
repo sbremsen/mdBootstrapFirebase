ccc44564e59c0eb18c290c01027efd18c7788b7e
Scott Remsen <X2SBREMS@southernco.com>
2021-05-25 14:08:56 -0500

    Modify connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase:  {
-  apiKey: "AIzaSyA7LAQOIKORzNCJFYwwWI9NjSBpYdHT9Es",
-  authDomain: "ngremsenbootstrap.firebaseapp.com",
-  projectId: "ngremsenbootstrap",
-  storageBucket: "ngremsenbootstrap.appspot.com",
-  messagingSenderId: "872507326058",
-  appId: "1:872507326058:web:aa7e132cb5fd2a32a81413",
-  measurementId: "G-XDN44M6PF9",
-  databaseURL: 'https://ngremsenbootstrap-default-rtdb.firebaseio.com/',
+
   }
 };
 
